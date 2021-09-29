0fe3eb7ce63fb4a704d7e93a2ef36d0515d37271
GoLabGitHub <50617669+GoLabGitHub@users.noreply.github.com>
2021-09-29 13:52:12 +0200

    Walking Algorithm

--- include/QuadrupedWalkingAlgorithm.hpp
+++ include/QuadrupedWalkingAlgorithm.hpp
@@ -36,8 +36,6 @@ class QuadrupedWalkingAlgorithm
 
     bool startLeg;
 
-    float time;
-    float pause;
     int nPoints;
     float zBodyPosition;
     float stepHeight;
@@ -45,6 +43,8 @@ class QuadrupedWalkingAlgorithm
     float xLengthR;
     float yLength;
     float rotation;
+
+    bool isReady;
 
     void calculateStepParameters    // calculate initial step parameters
     (
@@ -55,29 +55,31 @@ class QuadrupedWalkingAlgorithm
 
     void calculateStepPointXY    // calculate x and y cordinates of one point in step trajectory
     (
-        int pointNumber,    // number of point in trajectory
-        int pairLeg,        // pair of legs {-1, 1}
-        float xLength,      // one step lenght on x axis
-        float& xPoint,      // x cordinate of point in trajectory
-        float& yPoint,      // y cordinate of point in trajectory
-        LegParameters legParameters,
-        LegCordinates legCordinates
+        int pointNumber,                // number of point in trajectory
+        int pairLeg,                    // pair of legs {-1, 1}
+        float xLength,                  // one step lenght on x axis
+        float& xPoint,                  // x cordinate of point in trajectory
+        float& yPoint,                  // y cordinate of point in trajectory
+        LegParameters legParameters,    // struct
+        LegCordinates legCordinates     // struct
     );
+
+    float calculateZTrajectory(float xBeg, float yBeg, float x, float y, float lengthOfMove);    // calculate z of eliptic trajectory
 
     void calculateStepPoint    // calculate all cordinates of one point in step trajectory
     (
-        int pointNumber,     // number of point in trajectory
-        float xLength,       // one step lenght on x axis
-        bool isOnGround,     // leg is on ground
-        LegParameters legParameters,
-        LegCordinates legCordinates
+        int pointNumber,                // number of point in trajectory
+        float xLength,                  // one step lenght on x axis
+        bool isOnGround,                // leg is on ground
+        LegParameters legParameters,    // struct
+        LegCordinates legCordinates     // struct
     );
 
     public:
 
     QuadrupedWalkingAlgorithm();
 
-    void setLeftFrontMountingPosition(float x, float y);
+    void setLeftFrontMountingPosition(float x, float y);    // mounting position
     void setRightFrontMountingPosition(float x, float y);
     void setLeftBackMountingPosition(float x, float y);
     void setRightBackMountingPosition(float x, float y);
@@ -85,30 +87,26 @@ class QuadrupedWalkingAlgorithm
     void setSpacing(float xSpacing, float ySpacing);
     void setStartLeg(bool startLeg);
 
-    void setWalkParameters
+    bool setWalkParameters    // set parameters of step
     (	
-        float time,
-        float pause,
-        int nPoints,
-        float zBodyPosition,
-        float stepHeight,
-        float xLengthL,
-        float xLengthR,
-        float yLength,
-        float rotation
+        int nPoints,            // number of points in leg trajectory
+        float zBodyPosition,    // z axis body position
+        float stepHeight,       // height of step
+        float xLengthL,         // left legs one step lenght on x axis
+        float xLengthR,         // right legs one step length on x axis
+        float yLength,          // one step length on y axis
+        float rotation          // body rotation for one step
     );
 
-    void initStep();  // to przenieść do setWalkParameters
-
-    void setLeftFrontLegPosition(float x, float y, float z);
+    void setLeftFrontLegPosition(float x, float y, float z);    // set current position of legs (default [0,0,zBodyPosition])
     void setRightFrontLegPosition(float x, float y, float z);
     void setLeftBackLegPosition(float x, float y, float z);
     void setRightBackLegPosition(float x, float y, float z);
 
-    void calculate(float stepTime);
+    bool calculate(int poinNumber);    // calculate position for number of point in step
 
-    void getLeftFrontLegPosition(LegCordinates &legCordinates);
-    void getRightFrontLegPosition(LegCordinates &legCordinates);
-    void getLeftBackLegPosition(LegCordinates &legCordinates);
-    void getRightBackLegPosition(LegCordinates &legCordinates);
+    bool getLeftFrontLegPosition(LegCordinates &legCordinates);    // get goal cordinates of legs
+    bool getRightFrontLegPosition(LegCordinates &legCordinates);
+    bool getLeftBackLegPosition(LegCordinates &legCordinates);
+    bool getRightBackLegPosition(LegCordinates &legCordinates);
 };
